8c6b71beb7338d1e24d1208a6e2c5f4104654d64
Thomas VIGUÉ <thomas.vigue@etu.u-bordeaux.fr>
2017-04-20 17:35:44 +0200

    update SearchController

--- src/main/java/org/dogepool/practicalrx/controllers/SearchController.java
+++ src/main/java/org/dogepool/practicalrx/controllers/SearchController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import rx.Single;
+
 @RestController
 @RequestMapping(value = "/search", produces = MediaType.APPLICATION_JSON_VALUE)
 public class SearchController {
@@ -19,18 +21,18 @@ public class SearchController {
 	private SearchService service;
 
 	@RequestMapping("user/{pattern}")
-	public List<User> searchByName(@PathVariable String pattern) {
-		return service.findByName(pattern).toList().toBlocking().first();
+	public Single<List<User>> searchByName(@PathVariable String pattern) {
+		return service.findByName(pattern).toList().toSingle();
 	}
 
 	@RequestMapping("user/coins/{minCoins}")
-	public List<UserStat> searchByCoins(@PathVariable long minCoins) {
+	public Single<List<UserStat>> searchByCoins(@PathVariable long minCoins) {
 		return this.searchByCoins(minCoins, -1L);
 	}
 
 	@RequestMapping("user/coins/{minCoins}/{maxCoins}")
-	private List<UserStat> searchByCoins(@PathVariable long minCoins, @PathVariable long maxCoins) {
-		return service.findByCoins(minCoins, maxCoins).toList().toBlocking().first();
+	private Single<List<UserStat>> searchByCoins(@PathVariable long minCoins, @PathVariable long maxCoins) {
+		return service.findByCoins(minCoins, maxCoins).toList().toSingle();
 	}
 
 }
