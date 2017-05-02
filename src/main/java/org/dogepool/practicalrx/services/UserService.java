b48865be74bf509734adb552649e4809de9657a9
Thomas VIGUÉ <thomas.vigue@etu.u-bordeaux.fr>
2017-05-02 17:35:08 +0200

    asynchrone version of UserService RxJava1

--- src/main/java/org/dogepool/practicalrx/services/UserService.java
+++ src/main/java/org/dogepool/practicalrx/services/UserService.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.query.AsyncN1qlQueryResult;
+import com.couchbase.client.java.query.AsyncN1qlQueryRow;
 import com.couchbase.client.java.query.N1qlQueryResult;
 import com.couchbase.client.java.query.N1qlQueryRow;
 import com.couchbase.client.java.query.Select;
@@ -48,12 +50,10 @@ public class UserService {
 						.from(i(couchbaseBucket.name()))
 						.where(x("type")
 								.eq(s("user")))/* .groupBy(x("displayName")) */;
-				N1qlQueryResult queryResult = couchbaseBucket.query(statement);
-				List<User> users = new ArrayList<User>();
-				for (N1qlQueryRow qr : queryResult) {
-					users.add(User.fromJsonObject(qr.value()));
-				}
-				return Observable.from(users);
+				Observable<AsyncN1qlQueryResult> queryResult = couchbaseBucket.async().query(statement);
+				return queryResult.flatMap(AsyncN1qlQueryResult::rows)
+						.map(AsyncN1qlQueryRow::value)
+						.map(qr -> User.fromJsonObject(qr));
 			} catch (Exception e) {
 				return Observable.error(new DogePoolException("Error while getting list of users from database",
 						Error.DATABASE, HttpStatus.INTERNAL_SERVER_ERROR, e));
